e00955b51c89aefce2b4100fc80120e1b7c2929a
Daniel <daniel.m.adr@hotmail.com>
2024-03-30 16:19:52 -0300

    refactor: remove Category.java

--- src/main/java/com/InventoryManagementSystem/Application.java
+++ src/main/java/com/InventoryManagementSystem/Application.java
@@ -1,15 +1,10 @@
 package com.InventoryManagementSystem;
 
-import com.InventoryManagementSystem.service.CrudProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-  @Autowired
-  private CrudProductService crudProductService;
-
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
